ae688cb74c96ac35c7b9a13c8a095e386f2a760f
Pulkit <pulkitsingh302@gmail.com>
2021-02-10 19:02:55 +0530

    new up

--- gridsome.config.js
+++ gridsome.config.js
@@ -8,12 +8,18 @@ module.exports = {
   siteName: 'Pulkit',
   plugins: [
     {
-      use: '@gridsome/source-datocms',
+      use: "gridsome-source-graphql-prismic",
       options: {
-        apiToken: '847f0358846af3f2f4f3f8e098fb05', // required
-        previewMode: true,
-        apiUrl: 'https://site-api.datocms.com',
-        typeName: 'DatoCms'
+        url: "https://git-journal.prismic.io",
+        fieldName: 'prismicio',
+        typeName: 'prismicio',
+
+        headers: { 
+          'Prismic-Ref': `YCPWmBUAACQAsTY_`, // useMasterRef will overload this line
+          'Authorization': `MC5ZQ1BZckJVQUFDTUFzVF96.77-9Minvv73vv73vv73vv70POO-_ve-_ve-_vQbvv73vv73vv70LFO-_ve-_ve-_vQMhZu-_vXTvv71tZO-_ve-_ve-_vQ`,
+        },
+
+        useMasterRef: true // undefined by default
       }
     }
   ]
